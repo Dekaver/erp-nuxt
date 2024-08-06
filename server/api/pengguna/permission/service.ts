c33431648ac6627085e2a80fba4603474c2cd3f4
Dekaver <Alwiwahyudi8@gmail.com>
2024-08-06 20:22:32 +0800

    get permission string

--- server/api/pengguna/permission/service.ts
+++ server/api/pengguna/permission/service.ts
@@ -1,29 +1,43 @@
-import { eq } from "drizzle-orm";
-import { type NewPenggunaPermission, type PenggunaPermission, pengguna_permission } from "./schema";
+import { eq, sql } from 'drizzle-orm';
+import { type NewPenggunaPermission, type PenggunaPermission, PenggunaPermissionColumns, pengguna_permission } from './schema';
+import { permission } from '../../permission/schema';
 
 export const getPenggunaPermission = async (tx = db) => {
-	const data = await tx.select().from(pengguna_permission);
-	return data;
+    const data = await tx.select().from(pengguna_permission);
+    return data;
 };
 
-export const getPenggunaPermissionById = async (params: PenggunaPermission["id"], tx = db) => {
-	const data = await tx.select().from(pengguna_permission).where(eq(pengguna_permission.id, params));
-	return data;
+export const getPenggunaPermissionById = async (params: PenggunaPermission['id'], tx = db) => {
+    const data = await tx.select()
+        .from(pengguna_permission)
+        .where(eq(pengguna_permission.id, params));
+    return data;
+};
+
+export const getPenggunaPermissionStringById = async (params: PenggunaPermission['id'], tx = db) => {
+    const [data] = await tx
+        .select({ 
+			permission: sql<string>`STRING_AGG(${permission.permission}, ',')` 
+		})
+        .from(pengguna_permission)
+		.innerJoin(permission, eq(permission.id, pengguna_permission.id_permission))
+        .where(eq(pengguna_permission.id, params)).groupBy(pengguna_permission.id);
+    return data;
 };
 
 export const createPenggunaPermission = async (form: NewPenggunaPermission[], tx = db) => {
-	const data = await tx.insert(pengguna_permission).values(form).returning();
-	return data;
+    const data = await tx.insert(pengguna_permission).values(form).returning();
+    return data;
 };
 
-export const updatePenggunaPermission = async (params: PenggunaPermission["id"], form: NewPenggunaPermission[], tx = db) => {
-	return await tx.transaction(async (tx) => {
-		await deletePenggunaPermission(params, tx);
-		return await createPenggunaPermission(form, tx);
-	});
+export const updatePenggunaPermission = async (params: PenggunaPermission['id'], form: NewPenggunaPermission[], tx = db) => {
+    return await tx.transaction(async (tx) => {
+        await deletePenggunaPermission(params, tx);
+        return await createPenggunaPermission(form, tx);
+    });
 };
 
-export const deletePenggunaPermission = async (params: PenggunaPermission["id"], tx = db) => {
-	const data = await tx.delete(pengguna_permission).where(eq(pengguna_permission.id, params)).returning();
-	return data;
+export const deletePenggunaPermission = async (params: PenggunaPermission['id'], tx = db) => {
+    const data = await tx.delete(pengguna_permission).where(eq(pengguna_permission.id, params)).returning();
+    return data;
 };
