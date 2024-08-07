31787a3f860fc40b5082cb9506f17e921edbb715
Bakhtiar Ramadhan <bakhtiar.rf@gmail.com>
2024-08-07 16:21:42 +0800

    [CHG] Update Schema to new Folder

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
     dialect: 'postgresql',
-    schema: './server/api/**/schema.ts',
+    schema: './databases/**/schema.ts',
     out: './server/databases/migrations',
     dbCredentials: {
         url: process.env.DATABASE_URL as string,
