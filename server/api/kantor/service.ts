849bc516c341d05975f281b13e14387accf42be7
Bakhtiar Ramadhan <bakhtiar.rf@gmail.com>
2024-08-09 10:58:43 +0800

    [FIX] Error Showing Data

--- server/api/kantor/service.ts
+++ server/api/kantor/service.ts
@@ -2,33 +2,33 @@ import { sql, eq } from "drizzle-orm";
 import { type NewKantor, type Kantor, kantor, KantorColumns, UpdateKantor } from "@/databases/kantor/schema";
 
 export const getKantor = async (tx = db) => {
-    const data = await tx.execute(
+    const {rows} = await tx.execute(
         sql`
         SELECT a.*, b.name as akun_penjualan, c.name as akun_hutang, d.name as akun_piutang
-        FROM hr.kantor a 
+        FROM kantor a 
         INNER JOIN account b ON b.id=a.account_penjualan
         INNER JOIN account c ON c.id=a.account_hutang
         INNER JOIN account d ON d.id=a.account_piutang`
     );
+    return rows;
+};
+
+export const getKantorById = async (params: Kantor["id"], tx = db) => {
+    const [data] = await tx.select().from(kantor).where(eq(kantor.id, params));
     return data;
 };
 
-export const getKantorById = async (params: Kantor["id"]) => {
-    const [data] = await db.select().from(kantor).where(eq(kantor.id, params));
+export const createKantor = async (params: NewKantor, tx = db) => {
+    const [data] = await tx.insert(kantor).values(params).returning();
     return data;
 };
 
-export const createKantor = async (params: NewKantor) => {
-    const [data] = await db.insert(kantor).values(params).returning();
+export const updateKantor = async (params: Kantor["id"], form: UpdateKantor, tx = db) => {
+    const [data] = await tx.update(kantor).set(form).where(eq(kantor.id, params)).returning();
     return data;
 };
 
-export const updateKantor = async (params: Kantor["id"], form: UpdateKantor) => {
-    const [data] = await db.update(kantor).set(form).where(eq(kantor.id, params)).returning();
-    return data;
-};
-
-export const deleteKantor = async (id: Kantor["id"]) => {
-    const [data] = await db.delete(kantor).where(eq(kantor.id, id)).returning();
+export const deleteKantor = async (id: Kantor["id"], tx = db) => {
+    const [data] = await tx.delete(kantor).where(eq(kantor.id, id)).returning();
     return data;
 };
